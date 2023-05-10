d58e06e0a1a9665a4472247b20d8668d2fbd51e4
Ubuntu <ubuntu@ip-172-31-37-121.ap-northeast-2.compute.internal>
2023-05-10 02:00:38 +0000

    rbtree insert 구현

--- src/driver.c
+++ src/driver.c
@@ -1,5 +1,23 @@
 #include "rbtree.h"
+#include "rbtree.c"
+#include <stdio.h>
 
 int main(int argc, char *argv[]) {
+    rbtree *t = new_rbtree();
+
+    rbtree_insert(t, 8);
+    rbtree_insert(t, 7);
+    rbtree_insert(t, 9);
+    rbtree_insert(t, 3);
+    rbtree_insert(t, 6);
+    rbtree_insert(t, 4);
+    rbtree_insert(t, 5);
+    rbtree_insert(t, 12);
+
+    printf("%d\n",t->root->key);
+    printf("%d\n",t->root->color);
+    printf("%d\n",t->root->left->left->right->key);
+    printf("%d\n",t->root->left->left->right->color);
+
 
 }
